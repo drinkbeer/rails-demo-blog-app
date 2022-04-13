68d49cbb9ac59ba18c196a3dc2c1ccadbe4eaaab
Jianbin Chen <jason.chen@shopify.com>
2022-04-12 19:22:22 -0700

    Add GET, PUT, and DELETE controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,52 @@ class ArticlesController < ApplicationController
 
   def index
     puts "Hello from the articles controller"
+
+    @articles = Article.all
   end
 
   def show
-    @article_id = params[:id]
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show", as: "article_show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show", as: "article_show"
+  # Use resources to define routes for RESTful resources
+  resources :articles
 
   # Defines the root path route ("/")
   root "articles#index"
